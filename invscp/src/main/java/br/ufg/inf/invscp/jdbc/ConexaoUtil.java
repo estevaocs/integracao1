7253e22b59c01a174a7e1da2881b3d0a052323ca
Pedro Henrique <pedro.hc.silva@outlook.com>
2018-10-30 21:26:09 -0200

    Conexão BD remoto

--- invscp/src/main/java/br/ufg/inf/invscp/jdbc/ConexaoUtil.java
+++ invscp/src/main/java/br/ufg/inf/invscp/jdbc/ConexaoUtil.java
@@ -16,9 +16,48 @@ public class ConexaoUtil {
     }
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        return DriverManager.getConnection("jdbc:postgresql://localhost/invscp?user=postgres&password=123456");
+//        Class.forName("com.mysql.jdbc.Driver");
+//        return DriverManager.getConnection("jdbc:mysql://sql173.main-hosting.eu/u613749268_invsc?user=u613749268_grupo&password=grupo4321");
+
+        try{
+
+            String url = "jdbc:mysql://sql173.main-hosting.eu/u613749268_invsc";
+
+            String usuario = "u613749268_grupo";
+
+            String senha = "grupo4321";
+
+            Connection conexao = DriverManager.getConnection(url, usuario, senha);
+
+            return conexao;
+
+        }catch(SQLException sqle){
+
+            sqle.printStackTrace();
+
+        }
+
+        return null;
 
     }
 
+    public static void main(String[] args) {
+        ConexaoUtil conexaoUtil = new ConexaoUtil();
+        try {
+            Connection connection = conexaoUtil.getConnection();
+            boolean reachable = connection.isValid(10);
+            if (reachable) {
+                System.out.printf("1");
+            } else {
+                System.out.printf("0");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
+
